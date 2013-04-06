4f9c63eeb949be74c162db77807b2d09adc146bb
ptlis <ptlis@ptlis.net>
2013-04-06 12:33:15 +0100

    Added crude cli argument handling Added base composer.json Updated .gitignore

--- bin/import.php
+++ bin/import.php
@@ -1,42 +1,31 @@
 <?php
 
-$zipFile = '/home/brian/Desktop/codepo_gb.zip';
+require_once '../vendor/autoload.php';
 
-$archive = new ZipArchive();
+// Handle arguments
 
-if (($errorCode = $archive->open($zipFile) !== true)) {
-    switch ($errorCode) {
-        case ZipArchive::ER_EXISTS: // File already exists.
-            break;
-        case ZipArchive::ER_INCONS: // Zip archive inconsistent.
-            break;
-        case ZipArchive::ER_INVAL:  // Invalid argument.
-            break;
-        case ZipArchive::ER_MEMORY: // Malloc failure.
-            break;
-        case ZipArchive::ER_NOENT:  // No such file.
-            break;
-        case ZipArchive::ER_NOZIP:  // Not a zip archive.
-            break;
-        case ZipArchive::ER_OPEN:   //Can't open file.
-            break;
-        case ZipArchive::ER_READ:   // Read error.
-            break;
-        case ZipArchive::ER_SEEK:   // Seek error.
-            break;
+$fileName = null;
+
+if (count($argv) > 1) {
+
+    if ($argv[1] === '--help' || $argv[1] === 'help') {
+        // Help page
+
+        echo 'Usage:' . "\n";
+        echo '  ./import' . "\n";
+
+        echo "\n";
+    } elseif (substr($argv[count($argv) - 1], 0, 1) === '/') {
+        // The final element should be a path
+        $fileName = $argv[count($argv) - 1];
     }
+
 } else {
+    echo 'bad call'."\n\n";
+    die();
+}
 
-    // Get a list of files
-    for ($i = 0; $i < $archive->numFiles; $i++) {
-        $stat = $archive->statIndex($i);
-        $nameParts = array_filter(explode('/', $stat['name']));
-
-        if (count($nameParts) == 2 && $nameParts[0] == 'Data') {
-            $fileObj = new SplFileObject('zip://' . $zipFile . '#' . $stat['name']);
-            while (!$fileObj->eof()) {
-                echo print_r($fileObj->fgetcsv(), true)."\n";
-            }
-        }
-    }
+if ($fileName !== null) {
+    $CSOImporter = new \ptlis\CSOImport\CSOImport($argv[count($argv) - 1]);
+    $CSOImporter->extract();
 }
---
+++ src/ptlis/CSOImport/CSOImport.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+namespace ptlis\CSOImport;
+
+class CSOImport
+{
+    private $fileName;
+
+    public function __construct($fileName)
+    {
+        $this->fileName = $fileName;
+    }
+
+
+    public function extract()
+    {
+        $archive = new \ZipArchive();
+
+        if (($errorCode = $archive->open($this->fileName) !== true)) {
+            switch ($errorCode) {
+                case ZipArchive::ER_EXISTS: // File already exists.
+                    break;
+                case ZipArchive::ER_INCONS: // Zip archive inconsistent.
+                    break;
+                case ZipArchive::ER_INVAL:  // Invalid argument.
+                    break;
+                case ZipArchive::ER_MEMORY: // Malloc failure.
+                    break;
+                case ZipArchive::ER_NOENT:  // No such file.
+                    break;
+                case ZipArchive::ER_NOZIP:  // Not a zip archive.
+                    break;
+                case ZipArchive::ER_OPEN:   //Can't open file.
+                    break;
+                case ZipArchive::ER_READ:   // Read error.
+                    break;
+                case ZipArchive::ER_SEEK:   // Seek error.
+                    break;
+            }
+        } else {
+
+            // Get a list of files
+            for ($i = 0; $i < $archive->numFiles; $i++) {
+                $stat = $archive->statIndex($i);
+                $nameParts = array_filter(explode('/', $stat['name']));
+
+                if (count($nameParts) == 2 && $nameParts[0] == 'Data') {
+                    $fileObj = new \SplFileObject('zip://' . $this->fileName . '#' . $stat['name']);
+                    while (!$fileObj->eof()) {
+                        echo print_r($fileObj->fgetcsv(), true)."\n";
+                    }
+                }
+            }
+        }
+    }
+}
