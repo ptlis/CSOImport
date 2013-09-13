01f5b7b543a5e6849275e4a970a90d9c0e42c2cc
brian ridley <brian@beaker.magmadigital.co.uk>
2013-09-13 15:16:16 +0100

    Fix to symfony2 dependancy version no

--- bin/import.php
+++ bin/import.php
@@ -22,6 +22,12 @@ $app->register(
     )
 );
 
-$app->command(new ptlis\CSOImport\Command\ImportCommand($app['config']['import_map'], $app['config']['db_fields'], $app['config']['import_cols']));
+$app->command(
+    new ptlis\CSOImport\Command\ImportCommand(
+        $app['config']['import_map'],
+        $app['config']['db_fields'],
+        $app['config']['import_cols']
+    )
+);
 
 $app->run();
